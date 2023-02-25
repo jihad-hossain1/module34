1317a9f25142671fc9b66652d8c2cf923d8b73e4
jihad-hossain1 <jihadkhan934@gmail.com>
2023-02-25 07:00:12 +0600

    last change

--- api-example/practiceFile/quote.js
+++ api-example/practiceFile/quote.js
@@ -1,12 +1,24 @@
-const loadQuote = () =>{
+const loadQuote = () => {
     fetch('https://api.kanye.rest/')
-    .then(res => res.json())
-    .then(data => displayQuote(data))
+        .then(res => res.json())
+        .then(data => displayQuote(data))
+        .catch(error => console.log(error));  // error handeling
+}
+
+// async, await ... with error handleing  try, catch 
+const loadQuote2 = async () => {
+    try {
+        const res = await fetch('https://api.kanye.rest/');
+        const data = await res.json();
+        displayQuote(data)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const displayQuote = quote => {
     const blockQuote = document.getElementById('quote');
-    
+
     blockQuote.innerHTML = quote.quote;
 }
 
